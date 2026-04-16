2d6724b557aba8c27544403d35d63e10b9708268
Jonas Campos <jonas.campos.programacion@hotmail.com>
2026-04-16 21:38:08 +0200

    cambios en el config para hacer el multipagina

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './', // Usa rutas relativas en los enlaces generados en html
-  server: {
-    host: true, // Permite acceder desde cualquier dispositivo de tu red (responsive en móvil/tablet)
-    watch: {
-      usePolling: true, // Recomendado para WSL: detecta cambios en archivos
-    },
-  },
-});
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        location: 'location.html'
+      }
+    }
+  }
+})
